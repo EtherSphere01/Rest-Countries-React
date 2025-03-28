cb10adf9e79be28e7d082f7dfd49747bca91314b
Md. Naimur Rahman <naimurrahmanprogramming@gmail.com>
2025-03-28 21:56:52 +0600

    country added

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Country.css";
 
 const Country = ({ country }) => {
+  const [visited, setVisited] = useState(false);
   // console.log(country.flags.png);
+  const handleVisited = () => {
+    setVisited(!visited);
+  };
   return (
-    <div className="country">
+    <div className={`country ${visited ? `country-visited` : ""}`}>
       <h3>name : {country.name.common}</h3>
       <img src={country.flags.png} alt="" />
       <p> Independent : {country.independent ? "Free" : "Not Free"} </p>
       <p> Area : {country.area} </p>
       <p> Population : {country.population} </p>
       <p> Region : {country.region} </p>
+      <button onClick={handleVisited}>
+        {visited ? "Visited" : "Not Visited"}
+      </button>
     </div>
   );
 };
